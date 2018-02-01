13e5e451e63ae78043ca92842866d2830c772f20
Chloé Mezouar <chloe.mezouar@devinci.fr>
2018-02-01 04:40:36 +0100

    feat(michelin.js) scraping the prices

--- docs/js/michelin.js
+++ docs/js/michelin.js
@@ -1,73 +1,89 @@
 const request = require('request-promise'),
-     cheerio = require('cheerio'),
-     express = require('express'),
-     fs = require('fs');
+    cheerio = require('cheerio'),
+    express = require('express'),
+    fs = require('fs');
 
 var app = express();
 
-app.get('/scrape', function(req, res){
+app.get('/scrape', function (req, res) {
     //All the web scraping
-    url= 'https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin';
+    url = 'https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin';
 
     //the callback function takes 3 parameters, an error, response status code and the html
 
-    request(url, function(error, response, html){
+    request(url, function (error, response, html) {
         //check first if no errors occurred
 
-        if(!error){
-           var $ = cheerio.load(html);
-           
-           var name, stars, details, price_interval;
-           var json = [{name : " ", stars : " ", details : " ", price_interval : " "}];
-           //var restaurant={name : " ", stars : " ", details : " ", price_interval : " "};
+        if (!error) {
+            var $ = cheerio.load(html);
 
-           //using the unique div class as a starting point
-           $('.poi_card-display-title').filter(function(){
-
-            //console.log($(this));
-
-            var data = $(this);
-            
-            //console.log(data.text());
-
+            //var name, stars, details, price_interval;
+            var json = [{ name: "", price: "" }];
             var names = [];
-            names.push(data.text());
-            
-            names.forEach(element => {
-                console.log(element);
-               // restaurant.name = element;
-                var rest = {
-                    name : element
+            var prices = [];
 
+
+
+            //using the unique div class as a starting point
+            $('.poi_card-display-title ').filter(function () {
+
+                //console.log($(this));
+
+                var data = $(this);
+
+                //console.log(data.text());
+
+
+                names.push(data.text());
+            })
+
+            //scraping the prices
+            $('.poi_card-display-price').filter(function () {
+                var data = $(this);
+
+                prices.push(data.text());
+
+            })
+
+
+            names.forEach(n => {
+                var restaurant = {
+                    name: n,
+                    price: ""
                 };
-                json.push(rest);
+
+                json.push(restaurant);
             });
 
-            console.log(names);
+            prices.forEach(element => {
+                json.forEach(p => {
+                    p.price = element;
+                });
 
-           
+                fs.writeFile('starred_restaurants.json', JSON.stringify(json, null, 4), function (err) {
+                    console.log('File successfully written! - Check your project directory for the starred_restaurants.json file');
+                    res.send('Check your console');
+                });
 
-            fs.writeFile('starred_restaurants.json', JSON.stringify(json, null, 4), function(err){
-                console.log('File successfully written! - Check your project directory for the starred_restaurants.json file');   
-                res.send('Check your console');
-            })
-           })
+
+            });
+
         }
 
-        
+
 
 
     });
 
-    
-
-
-    
-})
 
 
 
-app.listen('8081', ()=> {
+
+});
+
+
+
+app.listen('8081', () => {
     console.log('Running server')
 });
 
